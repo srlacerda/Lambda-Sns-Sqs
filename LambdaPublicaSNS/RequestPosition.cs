b10681bcce9f7eea8cbb625837fde438ee1d2b1b
srlacerda <diego.lacerda.alves@gmail.com>
2020-11-25 16:09:02 -0300

    vai porra

---
+++ LambdaPublicaSNS/RequestPosition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaPublicaSNS
+{
+    public class RequestPosition
+    {
+        public int PositionId { get; set; }
+        public string Observacao { get; set; }
+    }
+}
